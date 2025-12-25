4e00b84634f8f80d1b8e46352ec0317875c8c4c2
Aya <[kent831217@gmail.com]>
2025-12-25 18:57:14 +0800

    feat: add aab key .jks

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+import java.util.Properties
+import java.io.FileInputStream
+
 android {
     namespace = "com.example.flutter_album_codex"
     compileSdk = flutter.compileSdkVersion
@@ -30,11 +33,21 @@ android {
         versionName = flutter.versionName
     }
 
+    val keystoreProperties = Properties()
+    val keystorePropertiesFile = rootProject.file("key.properties")
+    if (keystorePropertiesFile.exists()) {
+        keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.create("release") {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+            }
+            // isMinifyEnabled = false // 可以改 true 開啟壓縮混淆
         }
     }
 }
